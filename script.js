15f3a84ee858b4553b11813543036ae261f45d16
Ida Glen <idamglen@gmail.com>
2022-04-04 15:35:01 -0400

    add createGrid function and add basic css styles to divs

---
+++ script.js
@@ -0,0 +1,75 @@
+function createGrid(num) {
+    const container = document.getElementById('grid');
+    for (let i = 0; i < num; i++) {
+        let row = container.appendChild(document.createElement('div'));
+        for (let j = 0; j < num; j++) {
+            let coloumn = row.append(document.createElement('div'));
+        }
+    }
+}
+
+createGrid(4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+function createGrid(a) {
+    const container = document.getElementById('grid');    
+    for (let i = 0; i < a; i++) {
+        const row = container.appendChild(document.createElement('div'));
+        for (let j = 0; j < a; j++) {
+            const coloumn = row.appendChild(document.createElement('div'));
+        }
+    }
+}
+
+createGrid(4);
+
+
+function addSquare() {
+    const newSquare = document.createElement('div');
+    //Use the next two lines to add text to the div:
+    //const newContent = document.createTextNode('Insert text');
+    //newDiv.appendChild(newContent);
+    const container = document.getElementById('grid');
+    document.body.insertBefore(newSquare, container);
+}
+
+
+*/
